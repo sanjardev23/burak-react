aa74b89885edd5756adeca93dba7d4808284eb93
sanjardev23 <mksanjar04@gmail.com>
2026-06-30 17:37:01 +0900

    fix: modify homeNavbar header component

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import "../../../css/navbar.css";
 
 export function HomeNavbar() {
-  const authMember = true;
+  const authMember = null;
   return (
     <div className="home-navbar">
       <Container className="navbar-container">
@@ -75,7 +75,25 @@ export function HomeNavbar() {
             )}
           </Stack>
         </Stack>
-        <Stack>DETAIL</Stack>
+        <Stack className={"header-frame"}>
+          <Stack className={"detail"}>
+            <Box className={"head-main-txt"}>
+              World's Most Delicious Cousine
+            </Box>
+            <Box className={"wel-txt"}>The Choice, not just a choice</Box>
+            <Box className={"service-txt"}>24 hours service</Box>
+            <Box className={"signup"}>
+              {!authMember ? (
+                <Button variant={"contained"} className={"signup-button"}>
+                  SIGN UP
+                </Button>
+              ) : null}
+            </Box>
+          </Stack>
+          <Box className={"logo-frame"}>
+            <div className={"logo-img"}></div>
+          </Box>
+        </Stack>
       </Container>
     </div>
   );
