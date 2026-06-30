496ddd55737cba7234b7127d7a1160fa874ea279
sanjardev23 <mksanjar04@gmail.com>
2026-07-01 00:44:22 +0900

    feat: Z-TASK

--- src/train.ts
+++ src/train.ts
@@ -1,9 +1,21 @@
 // Y-TASK
-function findIntersection(arr1: number[], arr2: number[]): number[] {
-  return arr1.filter(item => arr2.includes(item));
+// function findIntersection(arr1: number[], arr2: number[]): number[] {
+//   return arr1.filter(item => arr2.includes(item));
+// }
+
+// console.log(findIntersection([1, 2, 3], [3, 2, 0]));  
+
+// Z-TASK
+function sumEvens(arr: number[]): number {
+  let sum = 0;
+
+  for (const num of arr) {
+    if (num % 2 === 0) {
+      sum += num;
+    }
+  }
+
+  return sum;
 }
 
-console.log(findIntersection([1, 2, 3], [3, 2, 0]));  
-
-
-// (yarn train to run)
+console.log(sumEvens([1, 2, 3, 5]));       
