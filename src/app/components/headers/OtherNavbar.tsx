08fd1e941cbd615972d5918dc4ea35fd86bc09b1
sanjardev23 <mksanjar04@gmail.com>
2026-06-30 17:47:25 +0900

    feat: publish otherNavbar header component

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,3 +1,75 @@
+import { Box, Button, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export function OtherNavbar() {
-    return <div>OtherNavbar</div>;
+  const authMember = null;
+  return (
+    <div className="other-navbar">
+      <Container className="navbar-container">
+        <Stack className="menu">
+          <Box>
+            <NavLink to="/">
+              <img className="brand-logo" src="/icons/burak.svg" />
+            </NavLink>
+          </Box>
+          <Stack className="links">
+            <Box className={"hover-line"}>
+              <NavLink to="/">Home</NavLink>
+            </Box>
+            <Box className={"hover-line"}>
+              <NavLink
+                to="/products"
+                className={({ isActive }) => (isActive ? "underline" : "")}
+              >
+                Products
+              </NavLink>
+            </Box>
+            {authMember ? (
+              <Box className={"hover-line"}>
+                <NavLink
+                  to="/orders"
+                  className={({ isActive }) => (isActive ? "underline" : "")}
+                >
+                  Orders
+                </NavLink>
+              </Box>
+            ) : null}
+            {authMember ? (
+              <Box className={"hover-line"}>
+                <NavLink
+                  to="/members-page"
+                  className={({ isActive }) => (isActive ? "underline" : "")}
+                >
+                  My Page
+                </NavLink>
+              </Box>
+            ) : null}
+            <Box className={"hover-line"}>
+              <NavLink
+                to="/help"
+                className={({ isActive }) => (isActive ? "underline" : "")}
+              >
+                Help
+              </NavLink>
+            </Box>
+            {/* BASKET */}
+
+            {!authMember ? (
+              <Box>
+                <Button variant="contained" className="login-button">
+                  Login
+                </Button>
+              </Box>
+            ) : (
+              <img
+                className="user-avatar"
+                src="/icons/default-user.svg"
+                aria-haspopup="true"
+              />
+            )}
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
