c92f6e06490828edba6d928a373e84b98e38f020
sanjardev23 <mksanjar04@gmail.com>
2026-07-23 14:40:31 +0900

    feat: create config & env variables

---
+++ src/lib/config.ts
@@ -0,0 +1,9 @@
+export const serverApi: string = import.meta.env.VITE_SERVER_API;
+
+export const Messages = {
+    error1: "Something went wrong!",
+    error2: "Please login first!",
+    error3: "Please fullfill all the inputs!",
+    error4: "Message is empty!",
+    error5: "Only images with jpeg, jpg or png format are allowed!",
+}
