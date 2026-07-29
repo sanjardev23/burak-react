4382fcaf50ef396be277b7a52464d8784e178199
sanjardev23 <mksanjar04@gmail.com>
2026-07-29 17:04:02 +0900

    feat: publish authentication modal component

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { store } from "./app/store";
 import App from "./app/App";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider as StylesThemeProvider } from "@mui/styles";
 import theme from "./app/MaterialTheme";
 import { Router } from "./app/Router";
 import "./css/index.css";
@@ -13,10 +14,12 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Router>
-          <App />
-        </Router>
+        <StylesThemeProvider theme={theme}>
+          <CssBaseline />
+          <Router>
+            <App />
+          </Router>
+        </StylesThemeProvider>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
