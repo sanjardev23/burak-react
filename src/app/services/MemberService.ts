deff2d82378203d23ca1f4dedd5baeb22617c706
sanjardev23 <mksanjar04@gmail.com>
2026-07-29 18:02:29 +0900

    feat: create signup process

--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { serverApi } from "../../lib/config";
-import type { Member } from "../../lib/types/member";
+import type { Member, MemberInput } from "../../lib/types/member";
 
 class MemberService {
   private readonly path: string;
@@ -33,6 +33,23 @@ class MemberService {
       return restaurant;
     } catch (err) {
       console.log("Error, getRestaurant:", err);
+      throw err;
+    }
+  }
+
+  public async signup(input: MemberInput): Promise<Member> {
+    try {
+      const url = this.path + "/member/signup";
+      const result = await axios.post(url, input, { withCredentials: true });
+      console.log("signup:", result);
+
+      const member: Member = result.data.member;
+      console.log("member:", member);
+      localStorage.setItem("memberData", JSON.stringify(member));
+
+      return member;
+    } catch (err) {
+      console.log("Error, signup:", err);
       throw err;
     }
   }
