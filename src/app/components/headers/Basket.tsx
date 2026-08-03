cc809239a46661dfb2d000b0c0c3d8343ac19d54
sanjardev23 <mksanjar04@gmail.com>
2026-08-03 17:47:53 +0900

    feat: develop order proceed business logic

--- src/app/components/headers/Basket.tsx
+++ src/app/components/headers/Basket.tsx
@@ -24,7 +24,7 @@ interface BasketProps {
 
 export default function Basket(props: BasketProps) {
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = props;
-  const { authMember } = useGlobals();
+  const { authMember, setOrderBuilder } = useGlobals();
   const history = useHistory();
   const itemsPrice: number = cartItems.reduce(
     (a: number, c: CartItem) => a + c.quantity * c.price,
@@ -56,6 +56,7 @@ export default function Basket(props: BasketProps) {
       onDeleteAll();
 
       //REFRESH VIA CONTEXT
+      setOrderBuilder(new Date());
       history.push("/orders");
     } catch (err) {
       console.log(err);
